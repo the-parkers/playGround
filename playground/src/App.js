30ed8fa8c23a3d5b77c5a1c8295e98ca1581d09e
Jarr1t <aliceajarrit@gmail.com>
2021-05-12 14:54:12 -0400

    added link between sign up and login pages

--- playground/src/App.js
+++ playground/src/App.js
@@ -1,12 +1,15 @@
 import './App.css';
 import LoginPage from './components/LoginPage';
 import SignUpPage from './components/SignUpPage';
+import {Route, Switch} from 'react-router-dom'
 
 function App() {
   return (
     <div className="App">
-       <SignUpPage/>
-       <LoginPage/>
+      <Switch>
+        <Route exact path='/' component={LoginPage}/>
+        <Route path='/signUp' component={SignUpPage}/>
+      </Switch>
     </div>
   );
 }
