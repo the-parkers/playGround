d3a8a628fb2303c8479877b742c2826cb21c0a99
LivCodes <oliviareeddev@gmail.com>
2021-05-17 11:07:30 -0400

    fetch login

--- playground/src/components/LoginPage.js
+++ playground/src/components/LoginPage.js
@@ -8,12 +8,31 @@ import {Link} from 'react-router-dom'
 function LoginPage() {
     const context = useContext(PlayGroundContext)
     const {email,password,setEmail,setPassword} = context
+    function handleSubmit(e) {
+        e.preventDefault()
+        const formData = {
+            email,
+            password
+        }
+        const option = {
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+                'Access-Control-Allow-Origin': '*'
+              },
+            method : 'POST',
+            body : JSON.stringify(formData)
+        }
+        fetch("http://localhost:5000/login", option)
+    }
     return (
        <div>
             <Logo/>
-            <UserInput type="email" value={email} setValue={setEmail} id="loginEmail" label="Email"/>
-            <UserInput type="password" value={password} setValue={setPassword} id="loginPassword" label="Password"/>
-            <Button className="loginButton" text="Login"/>
+            <form onSubmit={handleSubmit}>
+                <UserInput type="email" value={email} setValue={setEmail} id="loginEmail" label="Email"/>
+                <UserInput type="password" value={password} setValue={setPassword} id="loginPassword" label="Password"/>
+                <Button className="loginButton" text="Login"/>
+            </form>
             <br/>
             <span>Don't have an account?</span>
             <Link to={`/signUp`}>
