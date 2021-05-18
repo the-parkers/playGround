6678adf6c4530959582892eb917b06f0746c4b0c
LivCodes <oliviareeddev@gmail.com>
2021-05-18 15:29:44 -0400

    toggle & style fav/events

--- playground/src/components/UserPage.js
+++ playground/src/components/UserPage.js
@@ -1,5 +1,10 @@
 import Button from "./Button"
 import User from "./User"
+import { Route, Switch } from "react-router";
+import Favorites from "./Favorites";
+import Events from "./Events"
+import {Link} from 'react-router-dom'
+
 
 function UserPage() {
     const switchTab = () => {
@@ -8,9 +13,19 @@ function UserPage() {
     return (
         <div>
             <User src="https://semantic-ui.com/images/avatar/small/jenny.jpg" text="Test User"/>
-            <Button text="Favorites" className="favoriteTab" click={switchTab}/>
-            <Button text="Events" className="eventsTab" click={switchTab}/>
-            <div></div>
+            <Link to={`/favorites`}>
+                <Button text="Favorites" className="favoriteTab" click={switchTab}/> {' '}
+            </Link>
+            <Link to={`/events`}>
+                <Button text="Events" className="eventsTab" click={switchTab}/>
+            </Link>
+            <div>
+                
+                <Switch>
+                    <Route path={["/favorites"]} component={Favorites} />
+                    <Route path="/events" component={Events} />
+                </Switch>
+            </div>
             
         </div>
     )
