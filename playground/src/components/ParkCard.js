a489bac6da85459e2ea65282c2152ad7a420bf56
Jarr1t <aliceajarrit@gmail.com>
2021-05-19 15:59:34 -0400

    fixed park Cards

--- playground/src/components/ParkCard.js
+++ playground/src/components/ParkCard.js
@@ -31,25 +31,25 @@ function Parkcard(props){
     overAllAmen = Math.floor((amenRate.reduce((acc,cur) => acc + cur, 0)) / currentParkRating.length)        
     }
     return (
-            <Card className="park_cards" style={{ width: '18rem' }}>
+        <Link to={`/parks/${Park.id}`}>
+            <Card onClick={e => search('')} className="park_cards" style={{ width: '18rem' }}>
                 <Card.Img variant="top" src="https://upload.wikimedia.org/wikipedia/en/thumb/e/ed/Logo_of_the_New_York_City_Department_of_Parks_%26_Recreation.svg/1200px-Logo_of_the_New_York_City_Department_of_Parks_%26_Recreation.svg.png" />
+                <Card.Title onClick={e => search('')}>{Park.park_name}</Card.Title>
                 <Card.Body>
-                <Link to={`/parks/${Park.id}`}>
-                    <Card.Title onClick={e => search('')}>{Park.park_name}</Card.Title>
-                </Link>
                     <Card.Text>
                         {Park.park_location}
                     </Card.Text>
                     <Card.Subtitle>
                         {Park.subcategory}
                         <br/>
-                        <h3>Cleanliness: {overAllClean}</h3>
-                        <h3>Location: {overAllLoca}</h3>
-                        <h3>Amenities: {overAllAmen}</h3>
+                        <h4>Cleanliness: {overAllClean}</h4>
+                        <h4>Location: {overAllLoca}</h4>
+                        <h4>Amenities: {overAllAmen}</h4>
                         <Icon link name='heart outline' onClick={e => e.target.classList.value = 'heart link icon'}/>
                     </Card.Subtitle>
                 </Card.Body>
             </Card>
+        </Link>    
     )
 }
 
