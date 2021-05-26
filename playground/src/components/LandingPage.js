c06e20d13daab9b94a594d689e31831ff1526dd0
LivCodes <oliviareeddev@gmail.com>
2021-05-26 13:22:49 -0400

    landing animations

--- playground/src/components/LandingPage.js
+++ playground/src/components/LandingPage.js
@@ -21,13 +21,11 @@ function LandingPage() {
 
     return (
         <>
-            <Navbar className="nav">
-
+            <section className="showcase">
+            <div id="nav">
                     <h2 className="logo">PlayGround</h2>
                     <div className="toggle" onClick={addActive}></div>
-
-            </Navbar>
-            <section className="showcase">
+            </div>
                 <video src="/assets/scene2.mp4" loop autoPlay muted></video>
                 <div className="overlay"></div>
                 <div className="text">
@@ -37,23 +35,31 @@ function LandingPage() {
                     tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
                     quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
                     consequat.</p>
+                    <a href="/login">Explore</a>
                 </div>
-                <div className="social">
-                    <Link to="#">
-                        {/* <img src="https://i.ibb.co/x7P24fL/facebook.png"> */}
-                        <h1>menu item</h1>
-                    </Link>
-                </div>
+                <ul className="social">
+                   <li>
+                        <Link to="#" className="a">
+                            <img src="/assets/facebook.png" alt= ""/>
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to="#" className="a">
+                            <img src="/assets/twitter.png" alt= ""/>
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to="#" className="a">
+                            <img src="/assets/instagram.png" alt= ""/>
+                        </Link>
+                    </li>
+                </ul>
             </section>
             <div className="menu">
-                <Link to="/login">
-                    {/* <img src="https://i.ibb.co/x7P24fL/facebook.png"> */}
-                    <h1>Login</h1>
-                </Link>
-        
-                {/* <Link to="/signUp">
-                    <h1>Sign Up</h1>
-                </Link> */}
+                <ul>
+                    <li><Link to="/login" className="a">Login</Link></li>
+                    <li><Link to="/signUp" className="a">Sign Up</Link></li>
+                </ul>
             </div>
             
         </>
