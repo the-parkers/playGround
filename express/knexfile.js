bce0504992b4b69629224c34c846baa85b7f3ee5
segunsub <segunsubair3@gmail.com>
2021-05-28 10:25:39 -0400

    update to events

--- express/knexfile.js
+++ express/knexfile.js
@@ -19,18 +19,18 @@ module.exports = {
   },
 
   production: {
-    client: 'postgresql',
+    client: 'pg',
     // connection: process.env.DATABASE_URL,
-    connectionString: process.env.DATABASE_Url,
-    connection: {
-      user: process.env.DB_USERNAME,
-      database: process.env.DB_DATABASE,
-      password: process.env.DB_PASSWORD,
-      host: process.env.DB_HOSTNAME,
-      port: 5432,
-      ssl: { rejectUnauthorized: false }
-    },
-    ssl: { rejectUnauthorized: false },
+    connection: process.env.DATABASE_Url + `?ssl=true`,
+    // connection: {
+    //   user: process.env.DB_USERNAME,
+    //   database: process.env.DB_DATABASE,
+    //   password: process.env.DB_PASSWORD,
+    //   host: process.env.DB_HOSTNAME,
+    //   port: 5432,
+    //   ssl: { rejectUnauthorized: false }
+    // },
+ 
     pool: {
       min: 2,
       max: 10
